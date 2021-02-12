f6d774b476cc9a4023dd5d18a39a6408177d66ad
Coty Condry <ccondry@cisco.com>
2021-02-12 11:31:37 -0600

    log when service has started

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 
 // load main script
 const main = require('./main')
+const environment = require('./models/environment')
+const teamsLogger = require('./models/teams-logger')
 
 // check tokens every 10 minutes
 const throttle = 1000 * 60 * 10
@@ -12,3 +14,9 @@ main()
 
 // and run every throttle ms
 setInterval(main, throttle)
+
+// log service started
+const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development'
+const message = `${environment.name} version ${environment.version} service started on ${environment.hostname}.`
+console.log(message)
+teamsLogger.log(`service started on in ${mode} mode.`)
